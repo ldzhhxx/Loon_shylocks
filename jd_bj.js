4231188f34cf057d45518134cc08165b0661a8e8
Joinmogul <68455416+Joinmogul@users.noreply.github.com>
2021-01-28 09:38:32 +0800

    Update jd_bj.js

--- jd_bj.js
+++ jd_bj.js
@@ -32,7 +32,7 @@ if ($.isNode()) {
     cookiesArr.push(jdCookieNode[item])
   })
   if (process.env.JD_DEBUG && process.env.JD_DEBUG === 'false') console.log = () => {
-  };
+  };else {
   let cookiesData = $.getdata('CookiesJD') || "[]";
   cookiesData = jsonParse(cookiesData);
   cookiesArr = cookiesData.map(item => item.cookie);
@@ -40,7 +40,7 @@ if ($.isNode()) {
   cookiesArr.push(...[$.getdata('CookieJD2'), $.getdata('CookieJD')]);
   cookiesArr.reverse();
   cookiesArr = cookiesArr.filter(item => item !== "" && item !== null && item !== undefined);
-
+ }
 
 !(async () => {
   if (!cookiesArr[0]) {
